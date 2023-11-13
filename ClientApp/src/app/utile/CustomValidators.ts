eae79fc1e1ca86a5d28e908aefe3ccbeeabdea90
MaryAnzh <maryang@mail.ru>
2023-11-13 13:09:01 +0300

    feat: add date validator

--- ClientApp/src/app/utile/CustomValidators.ts
+++ ClientApp/src/app/utile/CustomValidators.ts
@@ -4,12 +4,15 @@ class CustomValidators {
 
   static dateValidators(): ValidatorFn {
     const sixteenYearInMS = +new Date('2020-01-01') - +new Date('2004-01-01');
+    const oneHundredYearsInMS = +new Date('2000-01-01') - +new Date('1900-01-01');
 
     return (control: AbstractControl): ValidationErrors | null => {
       const controlValue = control.value;
 
       const result = {
-        date: (value: string): boolean => Date.parse(value) > Date.now() - sixteenYearInMS,
+        date: (value: string): boolean =>
+          Date.now() - Date.parse(value) < sixteenYearInMS
+           || Date.now() - Date.parse(value) > (oneHundredYearsInMS),
       }
 
       const isValid = result.date(controlValue);
